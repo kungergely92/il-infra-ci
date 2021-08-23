cb58905a7f9af3c6064279c43ffbe0af269e77fc
Gergely Kun <kun.gergely.1992@gmail.com>
2021-08-23 14:58:08 +0200

    Provision ECR

---
+++ main.tf
@@ -0,0 +1,47 @@
+resource "aws_ecr_repository" "my_ecr_repo" {
+  name = var.name
+
+  image_tag_mutability = var.image_tag_mutability
+
+  image_scanning_configuration {
+    scan_on_push = var.scan_on_push
+  }
+}
+
+resource "aws_ecr_repository_policy" "my_ecr_repo_policy" {
+  repository = aws_ecr_repository.my_ecr_repo.name
+  policy     = data.aws_iam_policy_document.push_and_pull.json
+}
+
+data "aws_iam_policy_document" "push_and_pull" {
+
+  statement {
+    sid    = "ElasticContainerRegistryPushAndPull"
+    effect = "Allow"
+
+    principals {
+      identifiers = concat([local.current_account], local.push_and_pull_accounts)
+      type        = "AWS"
+    }
+
+    actions = [
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:BatchGetImage",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:BatchGetImage",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:PutImage",
+      "ecr:InitiateLayerUpload",
+      "ecr:UploadLayerPart",
+      "ecr:CompleteLayerUpload",
+    ]
+  }
+}
+
+locals {
+  push_and_pull_accounts = formatlist("arn:aws:iam::%s:root", var.push_and_pull_accounts)
+  current_account        = format("arn:aws:iam::%s:root", data.aws_caller_identity.current.account_id)
+}
+
+data "aws_caller_identity" "current" {}
---
+++ variables.tf
@@ -0,0 +1,39 @@
+variable "name" {
+  type        = string
+  description = "Name of the repository."
+}
+
+variable "tag_prefix_list" {
+  type        = list(string)
+  description = "List of image tag prefixes on which to take action with lifecycle policy."
+}
+
+variable "push_and_pull_accounts" {
+  default     = []
+  type        = list(string)
+  description = "AWS accounts which push and pull."
+}
+
+variable "max_untagged_image_count" {
+  default     = 1
+  type        = number
+  description = "The maximum number of untagged images that you want to retain in repository."
+}
+
+variable "max_tagged_image_count" {
+  default     = 30
+  type        = number
+  description = "The maximum number of tagged images that you want to retain in repository."
+}
+
+variable "scan_on_push" {
+  default     = false
+  type        = bool
+  description = "Whether images should automatically be scanned on push or not."
+}
+
+variable "image_tag_mutability" {
+  default     = "MUTABLE"
+  type        = string
+  description = "Whether images are allowed to overwrite existing tags."
+}
