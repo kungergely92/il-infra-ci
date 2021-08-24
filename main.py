8e80e1929e5fd916c69e610a2732dabdbf0af570
Gergely Kun <kun.gergely.1992@gmail.com>
2021-08-24 20:53:23 +0200

    Print engine version and disclaimer.

--- main.py
+++ main.py
@@ -7,28 +7,34 @@ ENDPOINT = os.environ["DB_URI"]
 DBNAME = os.environ["DB_NAME"]
 USER = os.environ["DB_USER"]
 PWD = os.environ["DB_PWD"]
-REGION = os.environ['DB_REGION']
-PORT = os.environ['DB_PORT']
+REGION = os.environ["DB_REGION"]
+PORT = os.environ["DB_PORT"]
 
 _logger = logging.getLogger()
 
+
 def connect_to_database():
-    _logger.info('Connecting to database...')
+    _logger.info("Connecting to database...")
     connection = None
     try:
         connection = psycopg2.connect(host=ENDPOINT, database=DBNAME, user=USER, password=PWD)
     except:
-        _logger.error('Unable to connect to database.')
+        _logger.error("Unable to connect to database.")
 
     return connection
 
+
 def print_connection_details():
     connection = connect_to_database()
-    _logger.info('Printing results...')
+    _logger.info("Printing results...")
     if connection is None:
-        print('Unable to connect to database.')
+        print("Unable to connect to database.")
     else:
-        print(f'{connection.info.dsn_parameters}')
+        rds_version_dict = {"engine_version": connection.server_version}
+        print(
+            f"{connection.info.dsn_parameters}\n {rds_version_dict}\n Credentials should be retrieved from SSM ParameterStore!"
+        )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     print_connection_details()
